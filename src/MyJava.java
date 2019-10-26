163c5fbdf2fd271e87b99d40321515b18669d2ab
pranishmac <pranish.mac321@gmail.com>
2019-10-26 12:59:53 +0530

    My first commit

---
+++ src/MyJava.java
@@ -0,0 +1,10 @@
+public class MyJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+        System.out.println("hi there");
+        System.out.println("my name is Pranish");
+    }
+
+}
